ae5fa36b85ee8b75c0d67cb8569756f0df31b121
HarishV14 <harishv@testpress.in>
2024-10-07 12:58:10 +0530

    Message is added to database

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,34 +1,37 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.db import database_sync_to_async
 from django.utils import timezone
+from .models import Message
+from courses.models import Course
 import uuid
+
+'''database operation should done in sync way
+   Websocket sending and reciving should done in async way nonblocking operation'''
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # getting current user
+        # Getting current user
         self.user = self.scope['user']
-        # getting cource id from url
+        # Getting course id from URL
         self.id = self.scope['url_route']['kwargs']['course_id']
-        self.room_group_name = 'chat_%s' % self.id
+        self.room_group_name = f'chat_{self.id}'
         
         # Join room group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         
         # Accept connection
         await self.accept()
         
+        # Send previous messages
+        previous_messages = await self.get_previous_messages()
+        for message in previous_messages:
+            await self.send(text_data=json.dumps(message))
+        
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
-    
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
@@ -40,10 +43,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def handle_message(self, message):
         now = timezone.now()
         message_id = str(uuid.uuid4())  # Generate a new UUID for the message
-        
+        await self.save_message(message_id, message, now)
+
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {
+            self.room_group_name,
+            {
                 'type': 'chat_message',
                 'message': message,
                 'message_id': message_id,
@@ -53,16 +58,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def handle_delete(self, message_id):
-        
-        # Send delete message to room group
-        # print("delete",message_id)
+        # Delete message and send delete event to room group
+        await self.delete_message(message_id)
         await self.channel_layer.group_send(
-            self.room_group_name, {
+            self.room_group_name,
+            {
                 'type': 'chat_delete',
                 'message_id': message_id,
             }
         )
-        
+
     async def chat_message(self, event):
         # Send message to WebSocket
         await self.send(text_data=json.dumps(event))
@@ -73,3 +78,38 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'type': 'delete',
             'message_id': event['message_id'],
         }))
+
+    async def save_message(self, message_id, message, now):
+        course = await database_sync_to_async(Course.objects.get)(id=self.id)
+        await database_sync_to_async(Message.objects.create)(
+            course=course,
+            user=self.user,
+            message=message,  
+            timestamp=now,
+            message_id=message_id,
+        )
+        
+    async def delete_message(self, message_id):
+        await database_sync_to_async(Message.objects.filter(message_id=message_id).update)(deleted=True)
+    
+    # we cannot force the synchronous process in async process we should handle async and sync both seperately
+    async def get_previous_messages(self):
+        # Fetch previous messages in an async context
+        messages = await database_sync_to_async(self.fetch_previous_messages)()
+        return messages
+
+    def fetch_previous_messages(self):
+        # This method runs in a synchronous context
+        messages = Message.objects.filter(course=self.id, deleted=False).order_by('-timestamp')[:50][::-1]
+        return [
+            {
+                'message_id': str(msg.message_id), 
+                'message': msg.message,
+                'user': msg.user.username,
+                'datetime': msg.timestamp.isoformat(),
+                'deleted': msg.deleted
+            } for msg in messages
+        ]
+
+
+    
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from courses.models import Course
+from django.contrib.auth.models import User
+from django.utils import timezone
+import uuid
 
 # Create your models here.
+class Message(models.Model):
+    
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    message_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    deleted = models.BooleanField(default=False)  # New field for soft deletion
+
+    def __str__(self):
+        return f"{self.user.username}: {self.message[:20]} ({self.timestamp})"
