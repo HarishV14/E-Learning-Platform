5ea6415222743ce9cd06def7377ea7785c75b013
HarishV14 <harishv@testpress.in>
2024-10-07 11:30:46 +0530

    Async way of chating in course is implemented

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,45 +1,48 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.utils import timezone
 import uuid
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # getting current user
         self.user = self.scope['user']
+        # getting cource id from url
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = 'chat_%s' % self.id
         
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         
         # Accept connection
-        self.accept()
+        await self.accept()
         
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
     
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
         if text_data_json['type'] == 'message':
-            self.handle_message(text_data_json['message'])
+            await self.handle_message(text_data_json['message'])
         elif text_data_json['type'] == 'delete':
-            self.handle_delete(text_data_json['message_id'])
+            await self.handle_delete(text_data_json['message_id'])
 
-    def handle_message(self, message):
+    async def handle_message(self, message):
         now = timezone.now()
         message_id = str(uuid.uuid4())  # Generate a new UUID for the message
         
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {
                 'type': 'chat_message',
                 'message': message,
@@ -49,22 +52,24 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    def handle_delete(self, message_id):
+    async def handle_delete(self, message_id):
+        
         # Send delete message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        # print("delete",message_id)
+        await self.channel_layer.group_send(
             self.room_group_name, {
                 'type': 'chat_delete',
                 'message_id': message_id,
             }
         )
         
-    def chat_message(self, event):
+    async def chat_message(self, event):
         # Send message to WebSocket
-        self.send(text_data=json.dumps(event))
+        await self.send(text_data=json.dumps(event))
 
-    def chat_delete(self, event):
+    async def chat_delete(self, event):
         # Send delete event to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'type': 'delete',
             'message_id': event['message_id'],
         }))
